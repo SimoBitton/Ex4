daa15b154645ae593f66e3980f8d25038f6dcd19
DeanZi <deanzion1@gmail.com>
2019-06-14 15:09:57 +0300

    connection works properly

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,19 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     EditText ed1,ed2;
-    Client client;
+    TcpClient mTcpClient;
     TextView IP, Port;
+    String ipAddress;
+    String portNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +32,53 @@ public class MainActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.button);
         ed1 = (EditText)findViewById(R.id.editText);
         ed2 = (EditText)findViewById(R.id.editText2);
-
         IP = (TextView)findViewById(R.id.textView);
         Port = (TextView)findViewById(R.id.textView2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ipAddress  = ed2.getText().toString();
+                portNumber = ed1.getText().toString();
+                new ConnectTask().execute("");
+                if (mTcpClient != null) {
+                    mTcpClient.sendMessage("testing");
+                }
 
-                client = new Client();
-                client.getInstance().connect();
                 Intent intent = new Intent(v.getContext(),SecondActivity.class);
                 startActivity(intent);
                 finish();
                 }
             });
         }
+
+    public class ConnectTask extends AsyncTask<String, String, TcpClient> {
+
+        @Override
+        protected TcpClient doInBackground(String... message) {
+
+            //we create a TCPClient object
+            mTcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run(ipAddress,portNumber);
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            //response received from server
+            Log.d("test", "response " + values[0]);
+            //process server response here....
+
+        }
+    }
+
     }
